47948e232e4df3035efc4571cea74dd536216d9f
Jean Carlo Nascimento <jnascimento@gmail.com>
2017-06-04 23:45:43 -0300

    Update watsonvr.js

--- watsonvr.js
+++ watsonvr.js
@@ -1,20 +1,19 @@
-let watson = require('watson-developer-cloud');
-let fs = require('fs');
+const watson = require('watson-developer-cloud');
+const fs = require('fs');
 
-let visual_recognition = watson.visual_recognition({
+const visual_recognition = watson.visual_recognition({
   api_key: 'e4b5f0635c00ae28629571bfadebdb651f00b4f2',
   version: 'v3',
   version_date: '2016-05-20'
 });
 
-let params = {
+const params = {
   images_file: fs.createReadStream('./carro-demo.jpg'),
   classifier_ids: ['veiculo']
 };
 
-visual_recognition.classify(params, function(err, res) {
-  if (err)
-    console.log(err);
-  else
-    console.log(JSON.stringify(res, null, 2));
-});
+visual_recognition.classify(params, (err, res) =>
+  (err)
+    ? console.log(err);
+    : console.log(JSON.stringify(res, null, 2));
+);
